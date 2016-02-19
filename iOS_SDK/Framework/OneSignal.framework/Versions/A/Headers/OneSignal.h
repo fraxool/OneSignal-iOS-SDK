bf093535ba5005d477eb13ecf8c2573959444052
Axel Hardy <me@axelhardy.com>
2016-02-19 10:30:30 +0100

    mAlwaysResetBadgeCount

--- iOS_SDK/Framework/OneSignal.framework/Versions/A/Headers/OneSignal.h
+++ iOS_SDK/Framework/OneSignal.framework/Versions/A/Headers/OneSignal.h
@@ -96,9 +96,9 @@ typedef NS_ENUM(NSUInteger, ONE_S_LOG_LEVEL) {
 
 - (void)IdsAvailable:(OneSignalIdsAvailableBlock)idsAvailableBlock;
 
-- (void)alwaysResetBadgeCount:(BOOL)enable;
 - (void)enableInAppAlertNotification:(BOOL)enable;
 - (void)setSubscription:(BOOL)enable;
+- (void)alwaysResetBadgeCount:(BOOL)enable;
 
 - (void)postNotification:(NSDictionary*)jsonData;
 - (void)postNotification:(NSDictionary*)jsonData onSuccess:(OneSignalResultSuccessBlock)successBlock onFailure:(OneSignalFailureBlock)failureBlock;
